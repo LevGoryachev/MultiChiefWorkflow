f91baaaf79a8494ac30b209bd8f19b136e97a350
LevGoryachev <l.goryachev@inbox.ru>
2022-01-14 14:31:00 +0300

    Create model interfaces (DTOs)

---
+++ src/main/java/ru/goryachev/multichief/workflow/model/dto/CommonDto.java
@@ -0,0 +1,8 @@
+package ru.goryachev.multichief.workflow.model.dto;
+/**
+ * CommonDto is an interface-marker for request-response DTOs.
+ * @author Lev Goryachev
+ * @version 1.1
+ */
+public interface CommonDto {
+}
---
+++ src/main/java/ru/goryachev/multichief/workflow/model/dto/PreformDto.java
@@ -0,0 +1,8 @@
+package ru.goryachev.multichief.workflow.model.dto;
+/**
+ * PreformDto is an interface-marker for response DTO.
+ * @author Lev Goryachev
+ * @version 1.1
+ */
+public interface PreformDto {
+}
